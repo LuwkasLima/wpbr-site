2595ccc3299452efd2a11e8a2a2d3afd7a6463e5
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-08-17 16:31:34 -0300

    Exemplo de configuração

---
+++ wp-config-dev.php
@@ -0,0 +1,35 @@
+<?php
+
+define('DB_NAME', 'wpbrasil');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+define('DOMAIN_CURRENT_SITE', 'wpbrasildev');
+
+define( 'AUTH_KEY', 'auth_key' );
+define( 'SECURE_AUTH_KEY', 'secure_uth_key' );
+define( 'LOGGED_IN_KEY', 'logged_in_key' );
+define( 'NONCE_KEY', 'nonce_key' );
+define( 'AUTH_SALT', 'auth_salt' );
+define( 'SECURE_AUTH_SALT', 'secure_auth_salt' );
+define( 'LOGGED_IN_SALT', 'logged_in_salt' );
+define( 'NONCE_SALT', 'nonce_salt' );
+
+$table_prefix  = 'wpbr_';
+
+define ('WPLANG', 'pt_BR');
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'wpbrasildev');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
